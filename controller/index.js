f4ee749df8c31a6a86ef61d52fc843984ca4c313
Bill Zissimopoulos <billziss@navimatics.com>
2018-06-21 10:49:47 -0700

    checkpoint

--- controller/index.js
+++ controller/index.js
@@ -73,7 +73,7 @@ const vmconf =
             [
                 {
                     "key": "startup-script",
-                    "value": package.config.FREEBSD_STARTX,
+                    "value": package.config.FREEBSD_BUILDER_STARTX,
                 },
             ],
         },
